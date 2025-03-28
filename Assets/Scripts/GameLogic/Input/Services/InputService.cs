67d75e0f938c132b552c65a8374df0a8fd71aa62
Aaron Worrall <aarondoesdev@gmail.com>
2025-03-28 14:26:01 -0400

    Accounting for input zero strategies
    Improved InputService to handle cases where there are zero listened inputs or zero responded inputs.

--- Assets/Scripts/GameLogic/Input/Services/InputService.cs
+++ Assets/Scripts/GameLogic/Input/Services/InputService.cs
@@ -21,7 +21,6 @@ namespace ESG.RockPaperScissors
         // This method resets the service state, priming it for the next complete set of inputs.
         public virtual void BeginListening()
         {
-            // TODO: Handle cases with 0 listened inputs or 0 responded inputs
             _remainingListenCount = _listeningInputStrategies.Length;
             _remainingResponseCount = _respondingInputStrategies.Length;
 
@@ -34,6 +33,37 @@ namespace ESG.RockPaperScissors
             {
                 _respondedInput[i] = HandSignal.None;
             }
+
+            CheckListenedInputs();
+        }
+
+        protected void CheckListenedInputs()
+        {
+            if(_remainingListenCount < 0)
+            {
+                Debug.LogWarning("More listened inputs received than expected - investigate InputService implementation");
+            }
+
+            // Once all listened input is received (if any), input can be requested from all of the 'responding' input strategies.
+            if(_remainingListenCount == 0)
+            {
+                PromptRespondingInputs();
+            }
+        }
+
+        protected void CheckRespondedInputs()
+        {
+            if(_remainingResponseCount < 0)
+            {
+                Debug.LogWarning("More responded inputs received than expected - investigate InputService implementation");
+            }
+
+            // Once all responding inputs have arrived (if any), the HandleAllInputReady method is invoked.
+            // This method must be defined in another non-abstract class implementing this one.
+            if(_remainingResponseCount == 0)
+            {
+                HandleAllInputReady();
+            }
         }
 
         protected abstract void CreateInputStrategies(int listeningCount, int respondingCount);
@@ -43,21 +73,14 @@ namespace ESG.RockPaperScissors
             _listenedInput[index] = signal;
             
             _remainingListenCount--;
-            if(_remainingListenCount < 0)
-            {
-                Debug.LogWarning("More listened inputs received than expected - investigate InputService implementation");
-            }
-            
-            // Once all listened input is received, input can be requested from all of the 'responding' input strategies.
-            if(_remainingListenCount == 0)
-            {
-                PromptRespondingInputs();
-            }
+            CheckListenedInputs();
         }
 
         // Iterates through all 'responding' input strategies and requests input from each.
         protected virtual void PromptRespondingInputs()
         {
+            CheckRespondedInputs(); // check for zero responded input case first
+
             for(int i = 0; i < _respondingInputStrategies.Length; i++)
             {
                 _respondingInputStrategies[i].RequestInput();
@@ -69,17 +92,7 @@ namespace ESG.RockPaperScissors
             _respondedInput[index] = signal;
             
             _remainingResponseCount--;
-            if(_remainingResponseCount < 0)
-            {
-                Debug.LogWarning("More responded inputs received than expected - investigate InputService implementation");
-            }
-
-            // Once all responding inputs have arrived, the HandleAllInputReady method is invoked.
-            // This method must be defined in another non-abstract class implementing this one.
-            if(_remainingResponseCount == 0)
-            {
-                HandleAllInputReady();
-            }
+            CheckRespondedInputs();
         }
 
         protected abstract void HandleAllInputReady();
